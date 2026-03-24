4530031c3309f1934393b95852823ab6adb9b169
Hermes <hermeschigoziri@gmail.com>
2026-03-24 04:50:16 +0100

    set up better-auth with prisma and neon

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,12 @@
-import { betterAuth } from 'better-auth'
-import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import {betterAuth} from 'better-auth'
+import {tanstackStartCookies} from 'better-auth/tanstack-start'
+import {prisma} from "#/db.ts";
+import {prismaAdapter} from "@better-auth/prisma-adapter";
 
 export const auth = betterAuth({
-  emailAndPassword: {
-    enabled: true,
-  },
-  plugins: [tanstackStartCookies()],
-})
+    emailAndPassword: {
+        enabled: true,
+    },
+    plugins: [tanstackStartCookies()],
+    database: prismaAdapter(prisma, {provider: "postgresql"})
+});
